3ad11dd6b73fef9fd7d12436d93815ec618fd340
Egon Hilgenstieler <egon@acras.com.br>
2020-08-14 15:03:55 -0300

    melhoria script de deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ git checkout deploy
 git pull
 git merge master
 bundle exec middleman build --clean
-echo "Agora comite as mudanças e volte para o master"
+echo "comitando as mudanças e volte para o master"
+git add -u
+git commit && git push && git checkout master
